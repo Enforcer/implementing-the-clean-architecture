da69a1fa455477fed1669ec9cba6eef4846b8c69
Sebastian Buczyński <nnplaya@gmail.com>
2021-09-17 22:58:40 +0200

    Configure alembic

---
+++ auctioning_platform/itca/db/migrations/env.py
@@ -0,0 +1,62 @@
+from alembic import context
+from sqlalchemy.engine import Engine
+
+from itca.db.base import metadata
+from itca.main import assemble
+
+config = context.config
+
+target_metadata = metadata
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    container = assemble(config.config_file_name)
+    engine = container.get(Engine)
+    context.configure(
+        url=engine.url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    container = assemble(config.config_file_name)
+    connectable = container.get(Engine)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
